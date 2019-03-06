2f21c22f989d238de0f3bdda4f13147a520d434f
Derrick Korir <derrickkip@Derricks-MacBook-Pro.local>
2019-03-06 10:34:35 +0300

    chore(setup): setup application structure
    - create basic folder structure
    - create sample route
    
    [Finishes #164399504]

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import 'dotenv/config';
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+})
+
+module.exports = app.listen(3000, () => {
+  console.log('Listening on port 3000');
+})
---
+++ src/tests/routes/home.test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const request = require('supertest');
+const app = require('../../index');
+
+chai.use(chaiHttp);
+
+describe('The / route', () => {
+  it('should return Ok status', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it('should return correct message', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, response) => {
+        response.text.should.contain('Hello World');
+        done()
+      });
+  });
+});
